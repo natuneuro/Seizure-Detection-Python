52dfd8ff7a761eb50917969b9aa206990bb0d8db
Clarissa <clarissalloures@gmail.com>
2020-06-22 19:23:57 -0300

    pagina de registro

---
+++ futuro.py
@@ -0,0 +1,189 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+
+# ------------  HOME SCREEN   ---------------------------------------------
+class MainWindow():
+    
+    def __init__(self,master,title,size):
+        self.master = master
+        self.title = title
+        self.resizable = (0,0)
+        self.size = size
+        self.master.title(self.title)
+        self.master.geometry(self.size)
+        self.master.config(bg="#DFEBE9")
+        self.canvas = 0
+        #self.master.iconbitmap("logos/icono.ico")
+        self.img = ImageTk.PhotoImage(Image.open("logos/cerebrito.png"))
+        Label(self.master, image=self.img, bg="#DFEBE9").place(x=64, y=55)
+   
+
+        self.logo_ufmg= Image.open("logos/ufmg _logo.png")
+        self.resized_ufmg= self.logo_ufmg.resize((100,42), Image.ANTIALIAS)
+        self.logo_ufmg_resized=ImageTk.PhotoImage(self.resized_ufmg)
+        Label(self.master, image=self.logo_ufmg_resized,bg="#DFEBE9").place(x=670,y=691)
+
+        self.logo_labbio = Image.open("logos/labbio_logo.png")
+        self.resized_labbio = self.logo_labbio.resize((100, 52), Image.ANTIALIAS)
+        self.logo_labbio_resized = ImageTk.PhotoImage(self.resized_labbio)
+        Label(self.master, image=self.logo_labbio_resized, bg="#DFEBE9").place(x=840, y=686)
+
+
+        self.quitMainWindow = Button(self.master,
+                                text="Close",
+                                width=15, height=1,
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                                cursor="hand2", 
+                                command=self.on_cancel).place(x=700, y=488)
+
+
+        self.aboutWindow = Button(self.master, 
+                                text="Abaut",
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                                width="15", height="1", cursor="hand2",
+                                command= self.aboutButtonClicked).place(x=700, y=101)
+
+        self.NewPacientWindow = Button(self.master,
+                                text="Add Pacient",
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                                width="15", height="1", cursor="hand2",
+                                command = self.newPacientButtonClicked).place(x=700, y=230)
+
+        self.OpenPacientWindow =Button(self.master, 
+                                text="Open Patient", 
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff", 
+                                width="15", height="1", cursor="hand2",
+                                command = self.openPacientButtonClicked).place(x=700, y=359)
+
+        # self. Boton_close = Button(self.master, text="Close", command=raiz.quit,
+        #                         font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+        #                         width="15", height="1", cursor="hand2").place(x=700, y=488)
+
+
+    def aboutButtonClicked(self):   
+        pageAbout = AboutWindow()
+
+    def newPacientButtonClicked(self):   
+        pageNew =  NewPacientWindow()
+
+    
+    def openPacientButtonClicked(self):   
+        pagePacient =  OpenPacientWindow()
+
+
+                    
+
+    def on_cancel(self):
+        self.master.destroy()
+
+
+# ------------------ Information Screen ------------
+class AboutWindow(Toplevel):
+    def __init__(self):
+        super().__init__(name='about page')
+        self.parent = "second window",
+        self.title("About")
+        self.config(bg="#DFEBE9")
+        self.size = "1024x768"
+        self.geometry("1024x768")
+
+        self.logo_cerebro_solo = Image.open("logos/cerebro_solo.png")
+        self.resized_cerebro = self.logo_cerebro_solo.resize((400, 400), Image.ANTIALIAS)
+        self.logo_cerebro_resized = ImageTk.PhotoImage(self.resized_cerebro)
+        Label(self, image=self.logo_cerebro_resized, bg="#DFEBE9").place(x=550, y=55)
+
+        self.logo_ufmg= Image.open("logos/ufmg _logo.png")
+        self.resized_ufmg= self.logo_ufmg.resize((150,63), Image.ANTIALIAS)
+        self.logo_ufmg_resized=ImageTk.PhotoImage(self.resized_ufmg)
+        Label(self, image=self.logo_ufmg_resized,bg="#DFEBE9").place(x=709, y=465)
+
+        self.logo_labbio = Image.open("logos/labbio_logo.png")
+        self.resized_labbio = self.logo_labbio.resize((150, 78), Image.ANTIALIAS)
+        self.logo_labbio_resized = ImageTk.PhotoImage(self.resized_labbio)
+        Label(self, image=self.logo_labbio_resized, bg="#DFEBE9").place(x=709, y=574)
+
+        
+        titulo_info = Label(self, text="About Project",
+                            font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
+                            fg="#14787A").place(x=10, y=20)
+
+        self.gobackButton = Button(self,
+                                text="Home",
+                                width=15, height=1,
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                                cursor="hand2", 
+                                command=self.on_cancel).place(x=700, y=700)
+        
+
+    def on_cancel(self):
+        self.destroy()
+
+
+
+
+# -----------------  Add Patient Screen ------------------------------
+class NewPacientWindow(Toplevel):
+    def __init__(self):
+        super().__init__(name='new pacient window')
+        self.parent = "second window",
+        self.title("About")
+        self.config(bg="#DFEBE9")
+        self.size = "1024x768"
+        self.geometry("1024x768")
+
+        #Label(self, image=logo_cerebro_resized, bg="#DFEBE9").place(x=550, y=55)
+        #Label(self, image=logo_ufmg_resized2, bg="#DFEBE9").place(x=709, y=465)
+        #Label(self, image=logo_labbio_resized2, bg="#DFEBE9").place(x=709, y=574)
+        titulo_info = Label(self, text="New Pacient",
+                            font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
+                            fg="#14787A").place(x=10, y=20)
+
+        self.gobackButton = Button(self,
+                                text="Home",
+                                width=15, height=1,
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                                cursor="hand2", 
+                                command=self.on_cancel).place(x=700, y=600)
+        
+
+    def on_cancel(self):
+        self.destroy()
+
+
+
+class OpenPacientWindow(Toplevel):
+    def __init__(self):
+        super().__init__(name='open pacients')
+        self.parent = "second window",
+        self.title("About")
+        self.config(bg="#DFEBE9" )
+        self.size = "1024x768"
+        self.geometry("1024x768")
+
+        #Label(self, image=logo_cerebro_resized, bg="#DFEBE9").place(x=550, y=55)
+        #Label(self, image=logo_ufmg_resized2, bg="#DFEBE9").place(x=709, y=465)
+        #Label(self, image=logo_labbio_resized2, bg="#DFEBE9").place(x=709, y=574)
+        titulo_info = Label(self, text="Open list pacients",
+                            font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
+                            fg="#14787A").place(x=10, y=20)
+
+        self.gobackButton = Button(self,
+                                text="Home",
+                                width=15, height=1,
+                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                                cursor="hand2", 
+                                command=self.on_cancel).place(x=700, y=600)
+        
+
+    def on_cancel(self):
+        self.destroy()
+
+
+
+if __name__ == "__main__":
+    mainWindow = Tk()
+    mainFenster = MainWindow(mainWindow, "Epilepsy Detection", "1024x768")
+    mainWindow.mainloop()
+
--- interfaz.py
+++ interfaz.py
@@ -1,214 +1,50 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import askopenfile
 from PIL import Image, ImageTk
 
-
-
 # ------------  HOME SCREEN   ---------------------------------------------
-class MainWindow():
-    
-    def __init__(self,master,title,size):
-        self.master = master
-        self.title = title
-        self.resizable = (0,0)
-        self.size = size
-        self.master.title(self.title)
-        self.master.geometry(self.size)
-        self.master.config(bg="#DFEBE9")
-        self.canvas = 0
-        #self.master.iconbitmap("logos/icono.ico")
-        self.img = ImageTk.PhotoImage(Image.open("logos/cerebrito.png"))
-        Label(self.master, image=self.img, bg="#DFEBE9").place(x=64, y=55)
-   
+raiz = Tk()
+raiz.title("Epilepsy Detection")
+raiz.resizable(0, 0)
+# raiz.iconbitmap("logos/icono.ico")
+raiz.geometry("1024x768")
+raiz.config(bg="#DFEBE9")
 
-        self.logo_ufmg= Image.open("logos/ufmg _logo.png")
-        self.resized_ufmg= self.logo_ufmg.resize((100,42), Image.ANTIALIAS)
-        self.logo_ufmg_resized=ImageTk.PhotoImage(self.resized_ufmg)
-        Label(self.master, image=self.logo_ufmg_resized,bg="#DFEBE9").place(x=670,y=691)
-
-        self.logo_labbio = Image.open("logos/labbio_logo.png")
-        self.resized_labbio = self.logo_labbio.resize((100, 52), Image.ANTIALIAS)
-        self.logo_labbio_resized = ImageTk.PhotoImage(self.resized_labbio)
-        Label(self.master, image=self.logo_labbio_resized, bg="#DFEBE9").place(x=840, y=686)
+# ------------------ IMAGES AND LOGOS INTO HOME SCREEN -------------------
 
 
-        self.quitMainWindow = Button(self.master,
-                                text="Close",
-                                width=15, height=1,
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-                                cursor="hand2", 
-                                command=self.on_cancel).place(x=700, y=488)
+logo_cerebro = PhotoImage(file="logos/cerebrito.png")
+Label(raiz, image=logo_cerebro, bg="#DFEBE9").place(x=64, y=55)
 
+logo_ufmg= Image.open("logos/ufmg _logo.png")
+resized_ufmg= logo_ufmg.resize((100,42), Image.ANTIALIAS)
+logo_ufmg_resized=ImageTk.PhotoImage(resized_ufmg)
+Label(raiz, image=logo_ufmg_resized,bg="#DFEBE9").place(x=670,y=691)
 
-        self.aboutWindow = Button(self.master, 
-                                text="Abaut",
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-                                width="15", height="1", cursor="hand2",
-                                command= self.aboutButtonClicked).place(x=700, y=101)
-
-        self.NewPacientWindow = Button(self.master,
-                                text="Add Pacient",
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-                                width="15", height="1", cursor="hand2",
-                                command = self.newPacientButtonClicked).place(x=700, y=230)
-
-        self.OpenPacientWindow =Button(self.master, 
-                                text="Open Patient", 
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff", 
-                                width="15", height="1", cursor="hand2",
-                                command = self.openPacientButtonClicked).place(x=700, y=359)
-
-        # self. Boton_close = Button(self.master, text="Close", command=raiz.quit,
-        #                         font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-        #                         width="15", height="1", cursor="hand2").place(x=700, y=488)
-
-
-    def aboutButtonClicked(self):   
-        pageAbout = AboutWindow()
-
-    def newPacientButtonClicked(self):   
-        pageNew =  NewPacientWindow()
-
-    
-    def openPacientButtonClicked(self):   
-        pagePacient =  OpenPacientWindow()
-
-
-                    
-
-    def on_cancel(self):
-        self.master.destroy()
-
-
-# ------------------ Information Screen ------------
-class AboutWindow(Toplevel):
-    def __init__(self):
-        super().__init__(name='about page')
-        self.parent = "second window",
-        self.title("About")
-        self.config(bg="#DFEBE9")
-        self.size = "1024x768"
-        self.geometry("1024x768")
-
-        self.logo_cerebro_solo = Image.open("logos/cerebro_solo.png")
-        self.resized_cerebro = self.logo_cerebro_solo.resize((400, 400), Image.ANTIALIAS)
-        self.logo_cerebro_resized = ImageTk.PhotoImage(self.resized_cerebro)
-        Label(self, image=self.logo_cerebro_resized, bg="#DFEBE9").place(x=550, y=55)
-
-        self.logo_ufmg= Image.open("logos/ufmg _logo.png")
-        self.resized_ufmg= self.logo_ufmg.resize((150,63), Image.ANTIALIAS)
-        self.logo_ufmg_resized=ImageTk.PhotoImage(self.resized_ufmg)
-        Label(self, image=self.logo_ufmg_resized,bg="#DFEBE9").place(x=709, y=465)
-
-        self.logo_labbio = Image.open("logos/labbio_logo.png")
-        self.resized_labbio = self.logo_labbio.resize((150, 78), Image.ANTIALIAS)
-        self.logo_labbio_resized = ImageTk.PhotoImage(self.resized_labbio)
-        Label(self, image=self.logo_labbio_resized, bg="#DFEBE9").place(x=709, y=574)
-
-        
-        titulo_info = Label(self, text="About Project",
-                            font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
-                            fg="#14787A").place(x=10, y=20)
-
-        self.gobackButton = Button(self,
-                                text="Home",
-                                width=15, height=1,
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-                                cursor="hand2", 
-                                command=self.on_cancel).place(x=700, y=700)
-        
-
-    def on_cancel(self):
-        self.destroy()
-
-
-
-
-# -----------------  Add Patient Screen ------------------------------
-class NewPacientWindow(Toplevel):
-    def __init__(self):
-        super().__init__(name='new pacient window')
-        self.parent = "second window",
-        self.title("About")
-        self.config(bg="#DFEBE9")
-        self.size = "1024x768"
-        self.geometry("1024x768")
-
-        #Label(self, image=logo_cerebro_resized, bg="#DFEBE9").place(x=550, y=55)
-        #Label(self, image=logo_ufmg_resized2, bg="#DFEBE9").place(x=709, y=465)
-        #Label(self, image=logo_labbio_resized2, bg="#DFEBE9").place(x=709, y=574)
-        titulo_info = Label(self, text="New Pacient",
-                            font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
-                            fg="#14787A").place(x=10, y=20)
-
-        self.gobackButton = Button(self,
-                                text="Home",
-                                width=15, height=1,
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-                                cursor="hand2", 
-                                command=self.on_cancel).place(x=700, y=600)
-        
-
-    def on_cancel(self):
-        self.destroy()
-
-
-
-class OpenPacientWindow(Toplevel):
-    def __init__(self):
-        super().__init__(name='open pacients')
-        self.parent = "second window",
-        self.title("About")
-        self.config(bg="#DFEBE9")
-        self.size = "1024x768"
-        self.geometry("1024x768")
-
-        #Label(self, image=logo_cerebro_resized, bg="#DFEBE9").place(x=550, y=55)
-        #Label(self, image=logo_ufmg_resized2, bg="#DFEBE9").place(x=709, y=465)
-        #Label(self, image=logo_labbio_resized2, bg="#DFEBE9").place(x=709, y=574)
-        titulo_info = Label(self, text="Open list pacients",
-                            font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
-                            fg="#14787A").place(x=10, y=20)
-
-        self.gobackButton = Button(self,
-                                text="Home",
-                                width=15, height=1,
-                                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
-                                cursor="hand2", 
-                                command=self.on_cancel).place(x=700, y=600)
-        
-
-    def on_cancel(self):
-        self.destroy()
-
-
-
-if __name__ == "__main__":
-    mainWindow = Tk()
-    mainFenster = MainWindow(mainWindow, "Epilepsy Detection", "1024x768")
-    mainWindow.mainloop()
-
-
-
+logo_labbio = Image.open("logos/labbio_logo.png")
+resized_labbio = logo_labbio.resize((100, 52), Image.ANTIALIAS)
+logo_labbio_resized = ImageTk.PhotoImage(resized_labbio)
+Label(raiz, image=logo_labbio_resized, bg="#DFEBE9").place(x=840, y=686)
 
 # ------------------ Images and logos into Information Screen ------------
 
-# logo_cerebro_solo = Image.open("logos/cerebro_solo.png")
-# resized_cerebro = logo_cerebro_solo.resize((400, 400), Image.ANTIALIAS)
-# logo_cerebro_resized = ImageTk.PhotoImage(resized_cerebro)
+logo_cerebro_solo = Image.open("logos/cerebro_solo.png")
+resized_cerebro = logo_cerebro_solo.resize((400, 400), Image.ANTIALIAS)
+logo_cerebro_resized = ImageTk.PhotoImage(resized_cerebro)
 
-# resized_ufmg2= logo_ufmg.resize((150,63), Image.ANTIALIAS)
-# logo_ufmg_resized2=ImageTk.PhotoImage(resized_ufmg2)
+resized_ufmg2= logo_ufmg.resize((150,63), Image.ANTIALIAS)
+logo_ufmg_resized2=ImageTk.PhotoImage(resized_ufmg2)
 
 
-# resized_labbio2 = logo_labbio.resize((150, 78), Image.ANTIALIAS)
-# logo_labbio_resized2 = ImageTk.PhotoImage(resized_labbio2)
+resized_labbio2 = logo_labbio.resize((150, 78), Image.ANTIALIAS)
+logo_labbio_resized2 = ImageTk.PhotoImage(resized_labbio2)
 
-# -----------------  Open Information Screen ------------------------------
 
 
 # -----------------  Open Information Screen ------------------------------
 def info():
-    raiz.withdraw()
+    #raiz.withdraw()
     Ventana_info = Toplevel()
     Ventana_info.geometry("1024x768")
     Ventana_info.config(bg="#DFEBE9")
@@ -219,36 +55,83 @@ def info():
     titulo_info = Label(Ventana_info, text="About Project",
                         font=("AvantGarde", 40, "bold"), bg="#DFEBE9",
                         fg="#14787A").place(x=10, y=20)
-    Boton_home = Button(Ventana_info, text="Home", command=Ventana_info.quit,
+    Boton_home = Button(Ventana_info, text="Home", command=Ventana_info.destroy,
                         font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
                         width="15", height="1", cursor="hand2").place(x=660, y=700)
 
 
 # -----------------  Open Add Patient Screen ------------------------------
 def add_patient():
-    raiz.withdraw()
+    #raiz.withdraw()
     Ventana_add = Toplevel()
     Ventana_add.geometry("1024x768")
     Ventana_add.config(bg="#DFEBE9")
     Ventana_add.title("Add Patient")
-    Boton_home = Button(Ventana_add, text="Home", command=open_patient,
+    a = Label(Ventana_add ,text = "File Name").grid(row = 0,column = 0)
+    b = Label(Ventana_add ,text = "Patient Age").grid(row = 1,column = 0)
+    c = Label(Ventana_add ,text = "F or M").grid(row = 2,column = 0)
+    d = Label(Ventana_add ,text = "Another information").grid(row = 3,column = 0)
+    a1 = Entry(Ventana_add).grid(row = 0,column = 1)
+    b1 = Entry(Ventana_add).grid(row = 1,column = 1)
+    c1 = Entry(Ventana_add).grid(row = 2,column = 1)
+    d1 = Entry(Ventana_add).grid(row = 3,column = 1)
+    btn2 = ttk.Button(Ventana_add ,text="Submit").grid(row=10,column=0)
+    #btn2 = ttk.Button(Ventana_add, text ='Open', command = lambda:open_file(),
+    #                font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+    #                width="15", height="1", cursor="hand2").place(x=700, y=359)
+   # btn.pack(side = TOP, pady = 10) 
+    def clicked():
+        res = "Welcome to " + txt.get()
+        lbl.configure(text= res)
+    # This function will be used to open 
+    # file in read mode and only Python files 
+    # will be opened 
+    def open_file(): 
+        file = askopenfile(mode ='r', filetypes =[('EGG', '*.py')]) 
+        if file is not None: 
+            content = file.read() 
+            print(content) 
+    
+    btn = ttk.Button(Ventana_add ,text="Add file", command = lambda:open_file()).grid(row=4,column=0)
+    Boton_home = Button(Ventana_add, text="Home", command=Ventana_add.destroy,
                         font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
                         width="15", height="1", cursor="hand2").place(x=700, y=700)
 
 
 # -----------------  Open Open Patient Screen ------------------------------
 def open_patient():
-    raiz.withdraw()
+    #raiz.withdraw()
     Ventana_open = Toplevel()
     Ventana_open.geometry("1024x768")
     Ventana_open.config(bg="#DFEBE9")
     Ventana_open.title("Open Patient")
-    Boton_home = Button(Ventana_open, text="Home", command=raiz,
+    Boton_home = Button(Ventana_open, text="Home", command=Ventana_open.destroy,
                         font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
                         width="15", height="1", cursor="hand2").place(x=700, y=700)
 
 
+# ---------------  Buttons into home screen ----------------------------------
+
+
+Boton_info = Button(raiz, text="Abaut", command=info,
+                    font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                    width="15", height="1", cursor="hand2").place(x=700, y=101)
+
+Boton_add = Button(raiz, text="Add Pacient", command=add_patient,
+                   font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                   width="15", height="1", cursor="hand2").place(x=700, y=230)
+
+Boton_open = Button(raiz, text="Open Patient", command=open_patient,
+                    font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                    width="15", height="1", cursor="hand2").place(x=700, y=359)
+
+Boton_close = Button(raiz, text="Close", command=raiz.quit,
+                     font=("AvantGarde", 20, "bold"), bg="#14787A", fg="#ffffff",
+                     width="15", height="1", cursor="hand2").place(x=700, y=488)
 
 
 
 
+
+
+raiz.mainloop()
